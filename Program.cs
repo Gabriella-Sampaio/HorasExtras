97be923c6ffa089760201d7ce8abad4232038859
Gabriella-Sampaio <dixxgabriellasam@gmail.com>
2025-05-16 22:22:57 -0300

    Início

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Escreva uma função que calcule o salário de um funcionário 
+a partir salário-hora base, (recebe quanto a pessoa recebe por
+hora no dia)
+o total de horas trabalhadas (recebe quanto a pessoa 
+trabalha normalmente)
+e número de horas-extras executadas. (recebe quantas horas extra
+a pessoa fez) 
+Considere que cada hora-extra é paga com acréscimo de 40%.
+
+faz o cálculo em uma subrotina
+
+idéia - fazer um programa q calcula de duas pessoas
+*/
+
+decimal salárioHoraBase;
+double horasTrabalhadas, horasExtra;
+
+Console.Clear();
+
