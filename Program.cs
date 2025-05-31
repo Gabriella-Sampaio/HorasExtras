ba177596075f6ae28d2e90beb7462803a5732981
Gabriella-Sampaio <dixxgabriellasam@gmail.com>
2025-05-31 00:33:26 -0300

    add prim subrotinas

--- Program.cs
+++ Program.cs
@@ -1,87 +1,49 @@
 ﻿Console.Clear();
 
-/*
-Escreva uma função que calcule o salário de um funcionário 
-a partir salário-hora base, (recebe quanto a pessoa recebe por
-hora no dia)
-o total de horas trabalhadas (recebe quanto a pessoa 
-trabalha normalmente)
-e número de horas-extras executadas. (recebe quantas horas extra
-a pessoa fez) 
-Considere que cada hora-extra é paga com acréscimo de 40%.
-
-faz o cálculo em uma subrotina
-
-idéia - fazer um programa q calcula de duas pessoas
-*/
-
-decimal salarioMensal;
-double horasDiaria, horasTotalDia;
-bool salarioMensalTrue, horasDiariaTrue;
+decimal salarioMensal, horasDiaria, horasTrabalhadas;
+const decimal acrescimoHrExtra = 1.4m;
 
 Console.WriteLine("Bem vindo, aqui você calcula suas horas extra do dia");
 
-Console.Write("\nQual seu salário mensal bruto?: "); //pegar e calcular o salário-hora base
-do
-{
-    salarioMensalTrue = Decimal.TryParse(Console.ReadLine(), out salarioMensal);
+salarioMensal = ValorDigitado("Qual seu salário mensal bruto?: ");
+horasDiaria = ValorDigitado("Quantas horas você trabalha por dia?: ");
+horasTrabalhadas = ValorDigitado("Quantas horas você trabalhou hoje? (incluindo horas extra): ");
 
-    if (!salarioMensalTrue)
-    {
-        Console.Write("\nValor inválido, tente novamente: ");
-    }
-}
-while (!salarioMensalTrue);
+decimal salarioHoraBase = salarioMensal / 30 / horasDiaria;
+decimal horasExtra = horasTrabalhadas - horasDiaria;
+decimal Total = salarioHoraBase * acrescimoHrExtra * horasExtra;
 
-// HorasDigitadas("teste: ", horasDiariaTrue, horasDiaria); // ???
-
-
-// Console.Write("\nQuantas horas você trabalha por mês?: ");
-// do
-// {
-//     horasDiariaTrue = Double.TryParse(Console.ReadLine(), out horasDiaria);
-//     if (!horasDiariaTrue)
-//     {
-//         Console.Write("\nValor inválido, tente novamente: ");
-//     }
-// }
-// while (!horasDiariaTrue);
-
-
-Console.Write("\nQuantas horas você trabalhou hoje no total? (incluindo horas extra): ");
-horasTotalDia = Convert.ToDouble(Console.ReadLine());
-
-
+Console.WriteLine($"{Total:C}");
 /*
 Fórmula
 
 Valores solicitados
-decimal salarioMensal
+
+decimal salarioMensal - ganha por mês
 double horasDiaria - quanto vc trabalha por dia
 double horasTotalDia - quanto vc trabalhou no dia
 
-double salárioHoraBase = (salarioMensal / 30) / horasTrabDiaria
+decimal salárioHoraBase = (salarioMensal / 30) / horasTrabDiaria
 
 horasExtra = horasTotalDia - horasDiaria  // pega quanto a pessoa trabalhou - o q normalmente trabalharia
 
-Total =  (salárioHoraBase * 1,4) * horasExtra 
+Total =  (salárioHoraBase * 1,4) * horasExtra
 
 
 */
 
 
 
-void HorasDigitadas (string mensagem, bool hrbool, double hr)
+
+
+decimal ValorDigitado (string mensagem)
 {
     Console.Write(mensagem);
-    do
+    decimal vlDig;
+    while (!Decimal.TryParse(Console.ReadLine(), out vlDig))
     {
-        hrbool = Double.TryParse(Console.ReadLine(), out hr);
-        if (!hrbool)
-        {
-            Console.Write("\nValor inválido, tente novamente: ");
-        }
+        Console.WriteLine("Valor inválido, tente novamente");
+        Console.Write(mensagem);
     }
-    while (!hrbool);
+    return vlDig;
 }
-
